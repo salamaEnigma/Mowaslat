ff5ad527cfe4644ff5807a83ee3962921f452f93
grayHatEnigma <salama92work@gmail.com>
2019-03-06 03:15:47 +0200

    Improving the Repository class

--- app/src/main/java/com/paramgy/mowaslatdemo/data/Repository.java
+++ app/src/main/java/com/paramgy/mowaslatdemo/data/Repository.java
@@ -10,12 +10,12 @@ import com.paramgy.mowaslatdemo.data.Result;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import androidx.lifecycle.LiveData;
 
 public class Repository {
     private AppDao appDAO;
-
 
     public Repository(Application application) {
         AppDatabase database = AppDatabase.getInstance(application);
@@ -26,53 +26,37 @@ public class Repository {
         return appDAO.getAllLocations();
     }
     public void deleteAllLocation() {
-        new DeleteAllLocationsAsyncTask(appDAO).execute();
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                appDAO.deleteAllLocations();
+            }
+        });
+
     }
     public void deleteAllResults() {
-        new DeleteAllResultsAsyncTask(appDAO).execute();
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                appDAO.deleteAllResults();
+            }
+        });
     }
 
 
     // THIS NEED TO BE REVIEWED
     // WE HAVE TO MAKE THE USER WAIT UNTIL THE RESULT IS FETCHED CORRECTLY (progress bar maybe with a timer)
-    public Result getResult(String current, String destination, int method) {
+    public Result getResult( String current,  String destination,  int method) {
         try {
-            return new GetResultAsyncTask(appDAO, current, destination, method).execute().get();
+            return new GetResultAsyncTask(appDAO,current,destination,method).execute().get();
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
     }
 
     //***************** Database Background Operations *******************//
-
-    private static class DeleteAllLocationsAsyncTask extends AsyncTask<Void, Void, Void> {
-        AppDao appDao;
-
-        public DeleteAllLocationsAsyncTask(AppDao appDao) {
-            this.appDao = appDao;
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            appDao.deleteAllLocations();
-            return null;
-        }
-    }// end DeleteAllLocationsAsyncTask
-
-    private static class DeleteAllResultsAsyncTask extends AsyncTask<Void, Void, Void> {
-        AppDao appDao;
-
-        public DeleteAllResultsAsyncTask(AppDao appDao) {
-            this.appDao = appDao;
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            appDao.deleteAllResults();
-            return null;
-        }
-    }// end DeleteAllResultsAsyncTask
 
     // THIS NEED TO BE REVIEWED
     private static class GetResultAsyncTask extends AsyncTask<Void, Void, Result> {
@@ -81,7 +65,7 @@ public class Repository {
         private String destination;
         private int method;
 
-        public GetResultAsyncTask(AppDao appDao, String current, String destination, int method) {
+        private GetResultAsyncTask(AppDao appDao, String current, String destination, int method) {
             this.appDao = appDao;
             this.current = current;
             this.destination = destination;
@@ -92,11 +76,6 @@ public class Repository {
         protected Result doInBackground(Void... voids) {
             return appDao.getResult(current, destination, method);
         }
-
-        @Override
-        protected void onPostExecute(Result result) {
-            super.onPostExecute(result);
-        }
     } // end GetResultAsyncTask
 
-}
+}// end Repository Class
