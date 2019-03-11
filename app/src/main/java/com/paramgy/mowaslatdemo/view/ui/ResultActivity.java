85279100372e1645f0797461661cadff17351b18
Enigma <mohamed92salama@gmail.com>
2019-03-11 22:50:14 +0200

    restructuring the project v5

--- app/src/main/java/com/paramgy/mowaslatdemo/view/ui/ResultActivity.java
+++ app/src/main/java/com/paramgy/mowaslatdemo/view/ui/ResultActivity.java
@@ -12,5 +12,11 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+    //Get extras ( result ) from the main activity
+
     }
+
+    //Has an oKButton method ;
+    //Has get user rating method  and push it to the database somehow .
 }
