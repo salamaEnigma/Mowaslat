0f6383a790ff1488fd6211e1e364e564010afe11
grayHatEnigma <salama92work@gmail.com>
2019-04-04 04:48:03 +0200

    fix a small bug in message avtivity

--- app/src/main/java/com/paramgy/mowaslat/features/message/view/MessageActivity.java
+++ app/src/main/java/com/paramgy/mowaslat/features/message/view/MessageActivity.java
@@ -40,15 +40,18 @@ public class MessageActivity extends AppCompatActivity implements MessageViewCon
             current = getIntent().getStringExtra("current");
             destination = getIntent().getStringExtra("destination");
             method = getIntent().getStringExtra("method");
-
             switch (method) {
                 case "car":
                     method = "مشروع";
+                    break;
                 case "train":
                     method = "قطر أبو قير";
+                    break;
                 case "tram":
                     method = "ترام";
+                    break;
             }
+
         }
     } // end on create
 
