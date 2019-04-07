861da59252c49f5b3989b87b1c6d0b6e229a891a
grayHatEnigma <salama92work@gmail.com>
2019-04-07 04:22:52 +0200

    Dagger2 initial commit

--- app/src/main/java/com/paramgy/mowaslat/data/model/pojos/NewResult.java
+++ app/src/main/java/com/paramgy/mowaslat/data/model/pojos/NewResult.java
@@ -6,6 +6,7 @@ public class NewResult {
 
     private String documentID;
     private String text;
+    private String [] textResults;
     private double time;
     private float cost;
     private static final String ERROR_MSG_NOT_FOUND = "لسه مضفناش ( المواصلة ) دي لقاعدة البيانات";
@@ -29,10 +30,19 @@ public class NewResult {
             return ERROR_MSG_NOT_FOUND;
         return text;
     }
-
     public void setText(String text) {
         this.text = text;
     }
+
+    public String[] getTextResults() {
+        return textResults;
+    }
+
+    public void setTextResults(String[] textResults) {
+        this.textResults = textResults;
+    }
+
+
 
     public double getTime() {
         return time;
